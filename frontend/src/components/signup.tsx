5bbca5abb93845f04dd79eb16bce3950efe8be43
Wojtek <tarchal@gmail.com>
2021-04-15 16:11:37 +0200

    Signup Form styled

--- frontend/src/components/signup.tsx
+++ frontend/src/components/signup.tsx
@@ -1,29 +1,37 @@
 import * as React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Grid, Segment, Button, Checkbox, Form } from 'semantic-ui-react';
 
 const SignupForm: React.FC = (): any => {
-    <Form>
-        <Form.Field>
-            <label>Login</label>
-            <input placeholder="Your username" />
-        </Form.Field>
-        <Form.Field>
-            <label>Password</label>
-            <input placeholder="Your password" />
-        </Form.Field>
-        <Form.Field>
-            <label>Confirm Password</label>
-            <input placeholder="Your password" />
-        </Form.Field>
-        <Form.Field>
-            <label>E-mail</label>
-            <input placeholder="example@domain.com" />
-        </Form.Field>
-        <Form.Field>
-            <Checkbox label="I agree to the Terms and Conditions" />
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-    </Form>;
+    return (
+        <Grid centered style={{ padding: 200, backgroundColor: '#582949' }}>
+            <Segment>
+                <Form>
+                    <Form.Field>
+                        <label>Login</label>
+                        <Form.Input id="login" placeholder="Your username" />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Password</label>
+                        <Form.Input id="password" type="password" placeholder="Your password" />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Confirm Password</label>
+                        <Form.Input id="passwordConf" type="password" placeholder="Your password" />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>E-mail</label>
+                        <Form.Input id="email" type="email" placeholder="default@example.com" />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox label="I agree withe the Privacy Policy" />
+                    </Form.Field>
+                    <Button type="submit" style={{ backgroundColor: '#1A9593', color: 'white' }}>
+                        Create account
+                    </Button>
+                </Form>
+            </Segment>
+        </Grid>
+    );
 };
 
 export default SignupForm;
--- frontend/src/containers/App.tsx
+++ frontend/src/containers/App.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Button } from 'semantic-ui-react';
+import SignupForm from '../components/signup';
 
 const App = (): React.ReactElement => {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
-            <Button primary content="Semantic works!" />
+            <SignupForm />
         </div>
     );
 };
